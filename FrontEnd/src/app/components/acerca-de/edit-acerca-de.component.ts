b08947704e5a9330641b8917849ef6ce37a83cd9
Alfburg <alfredburg@gmail.com>
2023-03-08 14:09:02 -0300

    frontend acerca de  firebase
    front end acerca de edit image

--- FrontEnd/src/app/components/acerca-de/edit-acerca-de.component.ts
+++ FrontEnd/src/app/components/acerca-de/edit-acerca-de.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { persona } from 'src/app/model/persona.model';
+import { ImageService } from 'src/app/service/image.service';
 import { PersonaService } from 'src/app/service/persona.service';
 
 @Component({
@@ -10,7 +11,8 @@ import { PersonaService } from 'src/app/service/persona.service';
 })
 export class EditAcercaDeComponent implements OnInit {
 persona: persona = null;
-  constructor(private activatedRouter: ActivatedRoute, private personaService: PersonaService, private router: Router ) {}
+  constructor(private activatedRouter: ActivatedRoute, private personaService: PersonaService, private router: Router, 
+    public imageService: ImageService) {}
 
   ngOnInit(): void {
     const id = this.activatedRouter.snapshot.params['id'];
@@ -22,10 +24,11 @@ persona: persona = null;
     this.router.navigate(['']);
   }
  )
-  }
+}
 
   onUpdate(): void{
     const id = this.activatedRouter.snapshot.params['id'];
+    this.persona.img = this.imageService.url
     this.personaService.update(id, this.persona).subscribe(
       data =>{
         this.router.navigate(['']);
@@ -34,10 +37,13 @@ persona: persona = null;
         this.router.navigate(['']);
       }
     )
-  }
+    }
 
   uploadImage($event:any){
-
+    const id = this.activatedRouter.snapshot.params['id'];
+    const name = "perfil_" + id;
+this.imageService.uploadImage($event, name)
+ 
   }
 
 }
---
+++ FrontEnd/src/environments/environment.development.ts
@@ -0,0 +1,14 @@
+export const environment = {
+  production: true,
+URL: 'http://localhost:8080/',
+
+  /*firebase: {
+    projectId: 'proyecto5bisfrontend',
+    appId: '1:786737780034:web:d15c380098dae2e692e4db',
+    storageBucket: 'proyecto5bisfrontend.appspot.com',
+    locationId: 'southamerica-east1',
+    apiKey: 'AIzaSyC7nLuEcgJVEjCNK3lgCnvf8SW6fG_mvFg',
+    authDomain: 'proyecto5bisfrontend.firebaseapp.com',
+    messagingSenderId: '786737780034',
+    measurementId: 'G-S97TR5G0GN',
+  },*/};
