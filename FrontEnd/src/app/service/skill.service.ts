b9dcb27d181f83309d24301992cf47f26afe0993
Alfburg <alfredburg@gmail.com>
2023-03-13 12:54:42 -0300

    cambio deploy
    cambio deploy ultimos

--- FrontEnd/src/app/service/skill.service.ts
+++ FrontEnd/src/app/service/skill.service.ts
@@ -7,7 +7,7 @@ import { Skill } from '../model/skill';
   providedIn: 'root'
 })
 export class SkillService {
-  URL = 'http://localhost:8080/skill/';
+  URL = 'https://abback.onrender.com/skill/';
   constructor(private httpClient: HttpClient) { }
 
   public lista(): Observable<Skill[]>{
